4c9d5f4d5482474ec8554d13144d2ff7a533d630
davidrgb <davidrussellrgb@gmail.com>
2022-12-10 14:11:59 -0600

    Finished guest login screen

--- lib/view/guest_login_screen.dart
+++ lib/view/guest_login_screen.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:twenty_twenty_questions/model/constant.dart';
+import 'package:twenty_twenty_questions/model/player.dart';
+import 'package:twenty_twenty_questions/view/lobby_list_screen.dart';
 
 class GuestLoginScreen extends StatefulWidget {
   static const routeName = '/guestLoginScreen';
@@ -11,6 +15,7 @@ class GuestLoginScreen extends StatefulWidget {
 
 class _GuestLoginScreenState extends State<GuestLoginScreen> {
   late _Controller controller;
+  GlobalKey<FormState> loginKey = GlobalKey();
 
   void render(fn) => setState(fn);
 
@@ -24,17 +29,66 @@ class _GuestLoginScreenState extends State<GuestLoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        padding: const EdgeInsets.all(50),
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Guest Login Screen'),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.only(
+            top: 50,
+          ),
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              const SizedBox(
+                width: 200,
+                child: Text(
+                  "Play as a guest.",
+                  style: TextStyle(
+                    fontSize: 36,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const SizedBox(
+                width: 200,
+                child: Divider(
+                  color: Colors.amber,
+                  thickness: 1,
+                ),
+              ),
+              const SizedBox(
+                height: 200,
+              ),
+              Form(
+                key: loginKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      height: 50,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.amber),
+                          ),
+                          hintText: "Username",
+                        ),
+                        style: const TextStyle(fontSize: 18),
+                        validator: controller.validateUsername,
+                        onSaved: controller.saveUsername,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: controller.login,
+                      child: const Text('Guest Login'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -43,5 +97,44 @@ class _GuestLoginScreenState extends State<GuestLoginScreen> {
 
 class _Controller {
   late _GuestLoginScreenState state;
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  String? username;
   _Controller(this.state);
+
+  String? validateUsername(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Username required';
+    } else if (value.length > 20) {
+      return 'Username must be 20 characters or less.';
+    } else {
+      return null;
+    }
+  }
+
+  void saveUsername(String? value) {
+    if (value != null) username = value;
+  }
+
+  void login() async {
+    FormState? currentState = state.loginKey.currentState;
+    if (currentState == null || !currentState.validate()) {
+      return;
+    }
+    currentState.save();
+
+    Player? player = Player(
+      playerID: username!,
+      friends: [],
+    );
+    await auth.signInAnonymously();
+
+    await Navigator.pushNamed(
+      state.context,
+      LobbyListScreen.routeName,
+      arguments: {
+        ARGS.PLAYER: player,
+      },
+    );
+  }
 }
