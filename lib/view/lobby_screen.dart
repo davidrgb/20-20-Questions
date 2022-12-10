99a3e63aac391a0dd3e573554da8efe49145fff3
davidrgb <davidrussellrgb@gmail.com>
2022-12-10 17:18:09 -0600

    Basic leave functionality for lobby

--- lib/view/lobby_screen.dart
+++ lib/view/lobby_screen.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:twenty_twenty_questions/controller/firebase_controller.dart';
 import 'package:twenty_twenty_questions/model/lobby.dart';
-import 'package:twenty_twenty_questions/model/player.dart';
-import 'package:twenty_twenty_questions/view/guest_login_screen.dart';
-import 'package:twenty_twenty_questions/view/player_login_screen.dart';
+import 'package:twenty_twenty_questions/model/profile.dart';
 
 class LobbyScreen extends StatefulWidget {
   static const routeName = '/lobbyScreen';
-  final Player player;
+  final Profile profile;
   final Lobby lobby;
 
   const LobbyScreen({
     Key? key,
-    required this.player,
+    required this.profile,
     required this.lobby,
   }) : super(key: key);
 
@@ -33,47 +32,70 @@ class _LobbyScreenState extends State<LobbyScreen> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () => Future.value(false),
+      onWillPop: controller.leave,
       child: Scaffold(
-        body: Container(
-          padding: const EdgeInsets.only(
-            top: 100,
-            bottom: 100,
-          ),
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              const SizedBox(
-                width: 200,
-                child: Text(
-                  "Press Start when all players have joined.",
-                  style: TextStyle(
-                    fontSize: 36,
+        appBar: AppBar(
+          title: Text(widget.lobby.name),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.only(
+              top: 25,
+              bottom: 50,
+            ),
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                const SizedBox(
+                  width: 200,
+                  child: Text(
+                    "Press Start when all players have joined.",
+                    style: TextStyle(
+                      fontSize: 36,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              const SizedBox(
-                width: 250,
-                child: Divider(
-                  color: Colors.amber,
-                  thickness: 2,
+                const SizedBox(height: 10),
+                const SizedBox(
+                  width: 250,
+                  child: Divider(
+                    color: Colors.amber,
+                    thickness: 2,
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: controller.start,
-                      child: const Text(
-                        'Start',
+                const SizedBox(
+                  height: 20,
+                ),
+                for (int i = 0; i < widget.lobby.players.length; i++)
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          widget.lobby.players[i].playerID,
+                          style: const TextStyle(fontSize: 18),
+                        ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(
+                        width: 200,
+                        child: Divider(
+                          color: Colors.amber,
+                          thickness: 1,
+                        ),
+                      )
+                    ],
+                  ),
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-            ],
+                ElevatedButton(
+                  onPressed: controller.start,
+                  child: const Text(
+                    'Start',
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -87,5 +109,14 @@ class _Controller {
 
   void start() async {}
 
-  void leave() async {}
+  Future<bool> leave() async {
+    for (int i = 0; i < state.widget.lobby.players.length; i++) {
+      if (state.widget.lobby.players[i].playerID == state.widget.profile.playerID) state.widget.lobby.players.removeAt(i);
+    }
+    List<Map<String, dynamic>> playerDocuments = state.widget.lobby.getPlayerDocumentList();
+    Map<String, dynamic> updateInfo = {};
+    updateInfo[Lobby.PLAYERS] = playerDocuments;
+    await FirestoreController.updateLobby(docID: state.widget.lobby.docID!, updateInfo: updateInfo);
+    return true;
+  }
 }
