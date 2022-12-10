c2c45c0d8879bef0970362f1df69d335a3d44d31
davidrgb <davidrussellrgb@gmail.com>
2022-12-10 14:12:15 -0600

    Finished layout of lobby list screen

--- lib/view/lobby_list_screen.dart
+++ lib/view/lobby_list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:twenty_twenty_questions/controller/firebase_controller.dart';
 import 'package:twenty_twenty_questions/model/lobby.dart';
@@ -17,6 +19,7 @@ class LobbyListScreen extends StatefulWidget {
 }
 
 class _LobbyListScreenState extends State<LobbyListScreen> {
+  GlobalKey<FormState> lobbyNameKey = GlobalKey();
   late _Controller controller;
 
   void render(fn) => setState(fn);
@@ -32,62 +35,116 @@ class _LobbyListScreenState extends State<LobbyListScreen> {
     return WillPopScope(
       onWillPop: () => Future.value(false),
       child: Scaffold(
-        body: Container(
-          padding: const EdgeInsets.only(
-            top: 50,
-          ),
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              const SizedBox(
-                width: 200,
-                child: Text(
-                  "Join \nor create \na lobby.",
-                  style: TextStyle(
-                    fontSize: 36,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.only(
+              top: 50,
+            ),
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                const SizedBox(
+                  width: 200,
+                  child: Text(
+                    "Join \nor create \na lobby.",
+                    style: TextStyle(
+                      fontSize: 36,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const SizedBox(
-                width: 200,
-                child: Divider(
-                  color: Colors.amber,
-                  thickness: 1,
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              controller.lobbies.isNotEmpty
-                  ? Expanded(
-                      child: ListView.separated(
-                        itemCount: controller.lobbies.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            color: Colors.amber,
-                            child: ListTile(
-                              title: Text(controller.lobbies[index].name),
-                              subtitle: Text(
-                                  'Created by ${controller.lobbies[index].hostID}'),
-                              onTap: () => controller.joinLobby(index),
+                const SizedBox(
+                  width: 200,
+                  child: Divider(
+                    color: Colors.amber,
+                    thickness: 1,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 200,
+                  child: Form(
+                    key: lobbyNameKey,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 150,
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.amber),
+                              ),
+                              hintText: "Create a lobby",
                             ),
-                          );
-                        },
-                        separatorBuilder: (context, index) => const Divider(
-                          color: Colors.amber,
+                            style: const TextStyle(fontSize: 18),
+                            validator: controller.validateLobbyName,
+                            onSaved: controller.saveLobbyName,
+                          ),
                         ),
-                      ),
-                    )
-                  : const Text(
-                      'No lobbies to display.',
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
+                        IconButton(
+                          onPressed: controller.createLobby,
+                          icon: const Icon(Icons.add),
+                        ),
+                      ],
                     ),
-            ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const SizedBox(
+                  width: 200,
+                  child: Divider(
+                    color: Colors.amber,
+                    thickness: 1,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                controller.lobbies.isNotEmpty
+                    ? RefreshIndicator(
+                        onRefresh: controller.refresh,
+                        child: ListView.separated(
+                          itemCount: controller.lobbies.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              color: Colors.amber,
+                              child: ListTile(
+                                title: Text(controller.lobbies[index].name),
+                                subtitle: Text(
+                                    'Created by ${controller.lobbies[index].hostID}'),
+                                onTap: () => controller.joinLobby(index),
+                              ),
+                            );
+                          },
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          separatorBuilder: (context, index) => const Divider(
+                            color: Colors.amber,
+                          ),
+                        ),
+                      )
+                    : Column(
+                        children: [
+                          const Text(
+                            'No lobbies to join.',
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: controller.refresh,
+                            child: const Text("Refresh"),
+                          ),
+                        ],
+                      ),
+              ],
+            ),
           ),
         ),
       ),
@@ -99,7 +156,10 @@ class _Controller {
   late _LobbyListScreenState state;
   List<Lobby> lobbies = [];
   bool isLoading = false;
+  String? lobbyName;
+
   _Controller(this.state) {
+    print(FirebaseAuth.instance.currentUser!.uid);
     refresh();
   }
 
@@ -119,6 +179,20 @@ class _Controller {
       print(e);
     }
   }
+
+  String? validateLobbyName(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Lobby name required.';
+    } else {
+      return null;
+    }
+  }
+
+  void saveLobbyName(String? value) {
+    if (value != null) lobbyName = value;
+  }
+
+  void createLobby() {}
 
   void joinLobby(int index) {}
 
