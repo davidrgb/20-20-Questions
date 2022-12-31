401c51886888813b88f045eb1cb62b4324a55e11
davidrgb <davidrussellrgb@gmail.com>
2022-12-31 17:04:01 -0600

    Added friend request collection

--- lib/model/constant.dart
+++ lib/model/constant.dart
@@ -2,6 +2,7 @@ class Constants {
   static const version = '1.0.4';
   static const lobbyCollection = 'lobbies';
   static const profileCollection = 'profiles';
+  static const friendRequestCollection = 'friend_requests';
   static const profilePicturesFolder = 'profile_pictures';
 }
 
